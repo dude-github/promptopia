bc9f8d9a17b3f985b305dc66b579233932f3cbec
Dude <dudeblogger1002gmail.com>
2024-03-08 13:51:37 +0530

    Fifth Commit: Implemented the Following Features: " 1. Established an API route for creating new prompts. 2. Developed an API route for fetching prompts. 3. Enhanced the Feed component to display all prompts effectively. 4. Updated the PromptCard component to enable prompt visualization. 5. Resolved a bug related to tags. " These updates refine the functionality and user experience of the application, ensuring smoother prompt creation, display, and interaction.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
   return (
-    <div>
-      <h1>Feed</h1>
-    </div>
+    <section className="feed">
+      <form action="" className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
   );
 };
 
--- models/prompt.js
+++ models/prompt.js
@@ -7,7 +7,11 @@ const PromptSchema = new Schema({
   },
   prompt: {
     type: String,
-    required: [true, "Tag is required"],
+    required: [true, "Prompt is required."],
+  },
+  tag: {
+    type: String,
+    required: [true, "Tag is required."],
   },
 });
 
