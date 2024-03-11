8b9d1ad696250530b5c5bfe556168540c05fae65
Dude <dudeblogger1002gmail.com>
2024-03-11 12:33:25 +0530

    Config  changes

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
